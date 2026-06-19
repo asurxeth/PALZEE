103a76a817b666d5c6db0d21bfa9045320f4d7cc
PRATHAM <prathamup123@gmail.com>
2026-06-20 03:25:46 +0530

    feat: add androidx compose material icons extended dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,6 +88,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation("androidx.compose.material:material-icons-extended")
     implementation(libs.androidx.compose.ui.text.google.fonts)
 
     // Hilt DI
