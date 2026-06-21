3526d645fd2d7e0a7207f1c5e5fa9e21018c2c97
PRATHAM <prathamup123@gmail.com>
2026-06-22 01:03:29 +0530

    Import androidx.compose.ui.draw.alpha in HomeScreen.kt

--- app/src/main/java/com/finrein/pals/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/finrein/pals/presentation/home/HomeScreen.kt
@@ -49,6 +49,7 @@ import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.rotate
